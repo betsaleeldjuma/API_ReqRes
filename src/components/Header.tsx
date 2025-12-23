a63e000fbd54e79150b564aa3fdf384e58e43b56
betsaleeldjuma <djumabestaleel@gmail.com>
2025-12-23 13:53:18 +0200

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div>
         <h1 className="text-3xl">API ReqRes</h1>
-        <input type="text" placeholder="Search..." className="border rounded-full p-1"/>
+        <input type="text" placeholder="Search..." className="border-b- focus:border-[#52616B] rounded-full pl-5 pb-1 pt-1"/>
     </div>
   )
 }
