34c5a2021916be057ecc6e00765a36b8bdb9a361
betsaleeldjuma <djumabestaleel@gmail.com>
2025-12-23 16:40:26 +0200

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {motion} from 'framer-motion'
 const Header = () => {
   return (
     <motion.div initial={{x: -50, scale: 0.4}} whileInView={{x: 0, scale: 1}}>
-        <h1 className="text-xl hover:text-5xl font-extrabold">API ReqRes</h1>
+        <h1 className="text-xl lg:text-5xl font-extrabold">API ReqRes</h1>
     </motion.div>
   )
 }
